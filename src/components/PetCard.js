df08fd21daa98d8f1c21ea7e303055be3cfa0185
CptnPluto <cptnpluto@gmail.com>
2025-01-16 13:21:04 +0200

    Fixed cat images not loading right, changed some small CSS on pet card.

--- src/components/PetCard.js
+++ src/components/PetCard.js
@@ -3,6 +3,11 @@ import NavButton from "./NavButton";
 
 const PetCard = ({ pet }) => {
 
+    // If the pet.picture URL start with `https://cataas.com//cat`, edit it to start with `https://cataas.com/cat` instead
+    if (pet.picture.startsWith("https://cataas.com//cat")) {
+        pet.picture = pet.picture.replace("https://cataas.com//cat", "https://cataas.com/cat");
+    }
+
     return (
         <div className="pet-card">
             <h3>{pet.name}</h3>
--- src/pages/Pet.js
+++ src/pages/Pet.js
@@ -8,271 +8,261 @@ import Modal from "../components/Modal";
 import PetForm from "../components/PetForm";
 
 const Pet = () => {
-    const queryStr = window.location.search;
-    const urlParams = new URLSearchParams(queryStr);
-    const petId = urlParams.get("id");
+	const queryStr = window.location.search;
+	const urlParams = new URLSearchParams(queryStr);
+	const petId = urlParams.get("id");
 
-    const [pet, setPet] = useState("");
-    const [show, setShow] = useState(false);
-    const [message, setMessage] = useState("");
-    const [render, setRender] = useState(false);
+	const [pet, setPet] = useState("");
+	const [show, setShow] = useState(false);
+	const [message, setMessage] = useState("");
+	const [render, setRender] = useState(false);
 
-    const { setSavedPetIds, savedPetIds, isSaved, setIsSaved } =
-        usePetsListContext();
-    const { user } = useAuthContext();
-    const navigate = useNavigate();
+	const { setSavedPetIds, savedPetIds, isSaved, setIsSaved } = usePetsListContext();
+	const { user } = useAuthContext();
+	const navigate = useNavigate();
 
-    const handleSubmit = () => {
-        setShow(false);
-        setRender(!render);
-    };
+	const handleSubmit = () => {
+		setShow(false);
+		setRender(!render);
+	};
 
-    const adoptOrFoster = async (e) => {
-        try {
-            const res = await axios.put(
-                `${process.env.REACT_APP_SERVER_URL}/pets/adopt/${petId}/${user.id}/${e.target.value}`,
-                null,
-                {
-                    withCredentials: true,
-                }
-            );
-            if (!res.data.ok) throw new Error("Error updating pet status");
-            if (savedPetIds.includes(petId)) {
-                setIsSaved(false);
-                const newSavedPets = savedPetIds.filter((id) => id !== petId);
-                setSavedPetIds(newSavedPets);
-            }
-            setRender(!render);
-        } catch (error) {
-            console.log(error);
-        }
-    };
+	const adoptOrFoster = async (e) => {
+		try {
+			const res = await axios.put(
+				`${process.env.REACT_APP_SERVER_URL}/pets/adopt/${petId}/${user.id}/${e.target.value}`,
+				null,
+				{
+					withCredentials: true,
+				}
+			);
+			if (!res.data.ok) throw new Error("Error updating pet status");
+			if (savedPetIds.includes(petId)) {
+				setIsSaved(false);
+				const newSavedPets = savedPetIds.filter((id) => id !== petId);
+				setSavedPetIds(newSavedPets);
+			}
+			setRender(!render);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-    const returnPet = async () => {
-        try {
-            const res = await axios.put(
-                `${process.env.REACT_APP_SERVER_URL}/pets/return/${petId}`,
-                null,
-                {
-                    withCredentials: true,
-                }
-            );
-            if (!res.data.ok) throw new Error("Error updating pet status");
-            setRender(!render);
-        } catch (error) {
-            console.log(error);
-        }
-    };
+	const returnPet = async () => {
+		try {
+			const res = await axios.put(
+				`${process.env.REACT_APP_SERVER_URL}/pets/return/${petId}`,
+				null,
+				{
+					withCredentials: true,
+				}
+			);
+			if (!res.data.ok) throw new Error("Error updating pet status");
+			setRender(!render);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-    const savePet = async (e) => {
-        try {
-            const res = await axios.put(
-                `${process.env.REACT_APP_SERVER_URL}/pets/save/${petId}/${user.id}`,
-                null,
-                { withCredentials: true }
-            );
-            if (!res.data.ok) throw new Error("Error updating pet status");
-            setIsSaved(true);
-            setRender(!render);
-        } catch (error) {
-            console.log(error);
-        }
-    };
+	const savePet = async (e) => {
+		try {
+			const res = await axios.put(
+				`${process.env.REACT_APP_SERVER_URL}/pets/save/${petId}/${user.id}`,
+				null,
+				{ withCredentials: true }
+			);
+			if (!res.data.ok) throw new Error("Error updating pet status");
+			setIsSaved(true);
+			setRender(!render);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-    const unSavePet = async (e) => {
-        try {
-            const res = await axios.put(
-                `${process.env.REACT_APP_SERVER_URL}/pets/unsave/${petId}/${user.id}`,
-                null,
-                {
-                    withCredentials: true,
-                }
-            );
-            if (!res.data.ok) throw new Error("Error updating pet status");
-            setIsSaved(false);
-        } catch (error) {
-            console.log(error);
-        }
-    };
+	const unSavePet = async (e) => {
+		try {
+			const res = await axios.put(
+				`${process.env.REACT_APP_SERVER_URL}/pets/unsave/${petId}/${user.id}`,
+				null,
+				{
+					withCredentials: true,
+				}
+			);
+			if (!res.data.ok) throw new Error("Error updating pet status");
+			setIsSaved(false);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-    const messageSet = (pet) => {
-        switch (true) {
-            case pet.adoptionStatus === "Adopted":
-                setMessage("This pet has been adopted");
-                break;
-            case pet.adoptionStatus === "Fostered":
-                setMessage("This pet has been fostered");
-                break;
-            case pet.adoptionStatus === "Available":
-                setMessage("This pet is available for adoption");
-                break;
-            default:
-                setMessage("");
-        }
-    };
+	const messageSet = (pet) => {
+		switch (true) {
+			case pet.adoptionStatus === "Adopted":
+				setMessage("This pet has been adopted");
+				break;
+			case pet.adoptionStatus === "Fostered":
+				setMessage("This pet has been fostered");
+				break;
+			case pet.adoptionStatus === "Available":
+				setMessage("This pet is available for adoption");
+				break;
+			default:
+				setMessage("");
+		}
+	};
 
-    useEffect(() => {
-        let isSubscribed = true;
+	// If the pet.picture URL start with `https://cataas.com//cat`, edit it to start with `https://cataas.com/cat` instead
+	if (pet && pet.picture.startsWith("https://cataas.com//cat")) {
+		pet.picture = pet.picture.replace("https://cataas.com//cat", "https://cataas.com/cat");
+	}
 
-        const fetchPet = async () => {
-            const response = await axios.get(
-                `${process.env.REACT_APP_SERVER_URL}/pets/${petId}`
-            );
-            const data = await response.data;
-            if (isSubscribed) setPet(data);
-            messageSet(data);
-            for (const id of savedPetIds) {
-                if (id === petId) {
-                    setIsSaved(true);
-                }
-            }
-        };
+	useEffect(() => {
+		let isSubscribed = true;
 
-        try {
-            fetchPet();
-        } catch (error) {
-            console.log(error);
-        }
+		const fetchPet = async () => {
+			const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/pets/${petId}`);
+			const data = await response.data;
+			if (isSubscribed) setPet(data);
+			messageSet(data);
+			for (const id of savedPetIds) {
+				if (id === petId) {
+					setIsSaved(true);
+				}
+			}
+		};
 
-        return () => (isSubscribed = false);
-    }, [user, savedPetIds, render, petId, setIsSaved]);
+		try {
+			fetchPet();
+		} catch (error) {
+			console.log(error);
+		}
 
-    console.log("Is saved: ", isSaved);
-    return (
-        <>
-            {!pet || !message ? (
-                <h1>Loading...</h1>
-            ) : (
-                <>
-                    {user && user.admin > 0 && (
-                        <button type="button" onClick={() => setShow(true)}>
-                            Edit Pet
-                        </button>
-                    )}
-                    <div className="pet-container">
-                        <img
-                            src={pet.picture}
-                            width="150"
-                            height="150"
-                            alt="pet"
-                        />
-                        <div className="details">
-                            <div className="label">Name: {pet.name}</div>
-                            <div className="label">Type: {pet.type}</div>
-                            <div className="label">Height: {pet.height}</div>
-                            <div className="label">Weight: {pet.weight}</div>
-                            <div className="label">Color: {pet.color}</div>
-                            <div className="label">Bio: {pet.bio}</div>
-                            <div className="label">
-                                Hypoallergenic: {pet.hypoallergenic}
-                            </div>
-                            <div className="label">
-                                Dietary Restrictions: {pet.dietaryRes}
-                            </div>
-                            <div className="label">Breed: {pet.breed}</div>
-                        </div>
-                        <div className="message_container">
-                            <div className="message">{message}</div>
-                        </div>
+		return () => (isSubscribed = false);
+	}, [user, savedPetIds, render, petId, setIsSaved]);
 
-                        <div className="changeStatus_buttons">
-                            {user && (
-                                <>
-                                    {pet.adoptionStatus === "Available" && (
-                                        <>
-                                            <button
-                                                type="button"
-                                                value="Adopted"
-                                                onClick={adoptOrFoster}
-                                                className="petButton"
-                                            >
-                                                Adopt
-                                            </button>
-                                            <button
-                                                type="button"
-                                                value="Fostered"
-                                                onClick={adoptOrFoster}
-                                                className="petButton"
-                                            >
-                                                Foster
-                                            </button>
-                                        </>
-                                    )}
-                                    {pet.ownerId === user.id ? (
-                                        <button
-                                            type="button"
-                                            onClick={returnPet}
-                                            value="Available"
-                                            className="petButton"
-                                        >
-                                            Return
-                                        </button>
-                                    ) : (
-                                        <>
-                                            {!savedPetIds.includes(
-                                                pet.petId
-                                            ) ? (
-                                                <button
-                                                    type="button"
-                                                    value="Saved"
-                                                    onClick={savePet}
-                                                    className="petButton"
-                                                >
-                                                    Save for Later
-                                                </button>
-                                            ) : (
-                                                <button
-                                                    type="button"
-                                                    onClick={unSavePet}
-                                                    className="petButton"
-                                                >
-                                                    UnSave Pet
-                                                </button>
-                                            )}
-                                        </>
-                                    )}
-                                </>
-                            )}
-                        </div>
-                        <div className="footer">
-                            {user && (
-                                <button
-                                    type="button"
-                                    onClick={() => navigate("/myPets")}
-                                    className="petButton"
-                                >
-                                    Back MyPets
-                                </button>
-                            )}
-                            <button
-                                type="button"
-                                onClick={() => navigate("/")}
-                                className="petButton"
-                            >
-                                Back to Home
-                            </button>
-                            <button
-                                type="button"
-                                onClick={() => navigate(-1)}
-                                className="petButton"
-                            >
-                                Back
-                            </button>
-                        </div>
+	console.log("Is saved: ", isSaved);
+	return (
+		<>
+			{!pet || !message ? (
+				<h1>Loading...</h1>
+			) : (
+				<>
+					{user && user.admin > 0 && (
+						<button type="button" onClick={() => setShow(true)}>
+							Edit Pet
+						</button>
+					)}
+					<div className="pet-container">
+						<img src={pet.picture} width="150" height="150" alt="pet" />
+						<div className="details">
+							<div className="label">Name: {pet.name}</div>
+							<div className="label">Type: {pet.type}</div>
+							<div className="label">Height: {pet.height}</div>
+							<div className="label">Weight: {pet.weight}</div>
+							<div className="label">Color: {pet.color}</div>
+							<div className="label">Bio: {pet.bio}</div>
+							<div className="label">
+								Hypoallergenic: {pet.hypoallergenic ? pet.hypoallergenic : "No"}
+							</div>
+							<div className="label">Dietary Restrictions: {pet.dietaryRes}</div>
+							<div className="label">Breed: {pet.breed}</div>
+						</div>
+						<div className="message_container">
+							<div className="message">{message}</div>
+						</div>
 
-                        {show && (
-                            <Modal show={show} onClose={() => setShow(false)}>
-                                <PetForm
-                                    handleSubmit={handleSubmit}
-                                    petData={pet}
-                                />
-                            </Modal>
-                        )}
-                    </div>
-                </>
-            )}
-        </>
-    );
+						<div className="changeStatus_buttons">
+							{user && (
+								<>
+									{pet.adoptionStatus === "Available" && (
+										<>
+											<button
+												type="button"
+												value="Adopted"
+												onClick={adoptOrFoster}
+												className="petButton"
+											>
+												Adopt
+											</button>
+											<button
+												type="button"
+												value="Fostered"
+												onClick={adoptOrFoster}
+												className="petButton"
+											>
+												Foster
+											</button>
+										</>
+									)}
+									{pet.ownerId === user.id ? (
+										<button
+											type="button"
+											onClick={returnPet}
+											value="Available"
+											className="petButton"
+										>
+											Return
+										</button>
+									) : (
+										<>
+											{!savedPetIds.includes(pet.petId) ? (
+												<button
+													type="button"
+													value="Saved"
+													onClick={savePet}
+													className="petButton"
+												>
+													Save for Later
+												</button>
+											) : (
+												<button
+													type="button"
+													onClick={unSavePet}
+													className="petButton"
+												>
+													UnSave Pet
+												</button>
+											)}
+										</>
+									)}
+								</>
+							)}
+						</div>
+						<div className="footer">
+							{user && (
+								<button
+									type="button"
+									onClick={() => navigate("/myPets")}
+									className="petButton"
+								>
+									Back MyPets
+								</button>
+							)}
+							<button
+								type="button"
+								onClick={() => navigate("/")}
+								className="petButton"
+							>
+								Back to Home
+							</button>
+							<button
+								type="button"
+								onClick={() => navigate(-1)}
+								className="petButton"
+							>
+								Back
+							</button>
+						</div>
+
+						{show && (
+							<Modal show={show} onClose={() => setShow(false)}>
+								<PetForm handleSubmit={handleSubmit} petData={pet} />
+							</Modal>
+						)}
+					</div>
+				</>
+			)}
+		</>
+	);
 };
 
 export default Pet;
